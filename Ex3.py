6440f28591ec68c56af4d1c168aed5aa2ddabb49
Ousmane9056 <tdiallo1@stud.fils.upb.ro>
2022-04-05 11:35:26 +0300

    J'ai fais l'exo 1,2,3

---
+++ Ex3.py
@@ -0,0 +1,19 @@
+mylist =[]
+
+mylist.append("Mange")
+mylist.append("Pomme")
+mylist.append("banane")
+mylist.append("Orange")
+mylist.append("Ananas")
+
+sorted_list = sorted(my_list)
+print(sorted_list)
+
+print("My_list")
+
+mylist.append("Bleu")
+mylist.append("Rouge")
+mylist.append("vert")
+
+mylist.reverse
+print (mylist)
---
+++ Exo1.py
@@ -0,0 +1,4 @@
+var1=12
+var2=17.9
+var3="SdE2"
+print(str(var1)+ str(var2)+SdE2)
